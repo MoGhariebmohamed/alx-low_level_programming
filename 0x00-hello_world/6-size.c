750c58384feb9d174198a451010f726361419741
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-10 15:41:05 -0700

    v0 cxx

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Entery point for this code
+ *
+ *Description: pusts for size of char
+ *Return: Always 0 (success)
+*/
+        int main(void)
+        {
+		printf("Size of a char:	%lu byte(s)\n",sizeof(char));
+		return (0);
+	}
