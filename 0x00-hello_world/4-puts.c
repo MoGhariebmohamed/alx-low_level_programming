2a662d32ced1db88a2b78dac6ed31df42153e7de
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-10 12:27:28 -0700

    v7 first

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main enter integr
- * pusts for cop the
- * return zero 
- */
+* main enter integr
+* pusts for cop the
+* return zero 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
