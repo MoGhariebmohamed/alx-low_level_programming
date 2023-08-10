d0deb8057d536597a1f0923be5464f6d4bb679b5
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-10 15:19:40 -0700

    v4 cd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main - enterypoint for this code
+ *Main - Enterypoint for this code
  *
- *description: pusts for cop the
- *return: always 0 (success)
+ *Description: pusts for cop the
+ *Return: Always 0 (success)
 */
 	int main(void)
 	{
