ddc1946283406d63028553de4729c64302a68b12
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-11 11:44:40 -0700

    v1 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,8 +5,12 @@
  *Description: pusts for size of char
  *Return: Always 0 (success)
 */
-        int main(void)
-        {
-		printf("Size of a char:	%lu byte(s)\n",sizeof(char));
-		return (0);
-	}
+int main(void)
+{
+	printf("Size of a char:%lu byte(s)\n", sizeof(char));
+	printf("Size of an int:%lu byte(s)\n", sizeof(int));
+	printf("Size of a long int:%lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int:%lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float:%lu byte(s)\n", sizeof(float));
+	return (0);
+}
