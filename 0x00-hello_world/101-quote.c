50fb77b7e67ce64fdc08ddbd247b9a57d5c6b8e1
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-11 12:27:05 -0700

    v0 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entery to the code
+ *
+ *
+ *
+ * return: 1 success
+ */
+int main(void)
+{
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	return (1);
+}
