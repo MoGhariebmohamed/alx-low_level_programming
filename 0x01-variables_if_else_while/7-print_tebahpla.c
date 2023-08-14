ab927fc9a7ec201c355b067d6b8751fe285f958e
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-14 06:09:24 -0700

    v0 t7 revalpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - the main function here
+ *
+ *description: the printing for charachter alpha
+ *Return: 0 sucess
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
