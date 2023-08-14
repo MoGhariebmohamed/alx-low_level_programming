b348ce8ab750e42d976a461884e2bbb1d31a459e
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-14 01:53:20 -0700

    v0 t2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - the main function here
+ *
+ *description: the printing for charachter alpha
+ *Return: 0 sucess
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	putchar(c);
+	putchar('\n');
+	}
+	return (0);
+}
