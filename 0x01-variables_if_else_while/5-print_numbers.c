e83163d722e901c84dc314bdef403a2fe47e78d1
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-14 04:24:01 -0700

    v0 t5 no

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - the main function here
+ *
+ *description: the printing for charachter alpha
+ *Return: 0 sucess
+ */
+int main(void)
+{
+	int no;
+
+	for (no = 0; no < 10; no++)
+	{
+		putchar(no);
+	}
+	putchar('\n');
+	return (0);
+}
