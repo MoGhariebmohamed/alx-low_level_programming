4dfeaaf9f484b2db4b9b56b39ed2ffde309a4bb0
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-15 13:42:35 -0700

    v1 0put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,24 @@
+#include <main.h>
+
 /**
- *main - the main function to print
+ *main - the main function to print putchar
  *
  *description: this function to print putchar
  *Return: 0 sucess
  */
 int main(void)
+
 {
-	putchar('_putchar')
+	char name[] = "_putchar";
+
+		int i = 0i;
+
+		while (i < 8)
+
+		{
+			_putchar(name[i]);
+			i++;
+		}
+		_putchar('\n');
 		return (0);
 }
