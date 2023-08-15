3088954a4dba11cd9bd0e46f526c16395415602c
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-15 09:11:24 -0700

    v0 trial

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+/**
+ *main - the main function to print
+ *
+ *description: this function to print putchar
+ *Return: 0 sucess
+ */
+int main(void)
+{
+	putchar('_putchar')
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+this directory for applying nested loop fn with using char only
