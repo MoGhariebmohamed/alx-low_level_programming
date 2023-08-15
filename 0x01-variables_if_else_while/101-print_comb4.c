140f73a64224ee6f182ce22c05d1a1c5d8b61de1
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-14 22:48:48 -0700

    v0 t101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ *main - the main function here
+ *
+ *description: the printing for charachter alpha
+ *Return: 0 sucess
+ */
+int main(void)
+{
+	int no = 0;
+	int no2;
+	int no3;
+
+	while (no < 10)
+	{
+		for (no2 = no + 1; no2 < 10; no2++)
+		{
+			for (no3 = no2 +1; no3 < 10; no3++)
+			{
+		putchar(no + '0');
+		putchar(no2 + '0');
+		putchar(no3 + '0');
+		if (no != 8 || no2 != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+			}
+		}
+		no++;
+	}
+	putchar('\n');
+	return (0);
+}
