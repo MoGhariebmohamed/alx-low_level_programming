d94586db1aa67939f3d788aa3b9ac46e0172bcdd
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-15 19:49:14 -0700

    v0 t9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *times_table - the main function to print time table
+ *
+ *description: this function to print putchar
+ *Return: 0 sucess
+ */
+void times_table(void)
+{
+int n = 0;
+int i;
+while (n <= 9)
+{
+	i = 0;
+	while (i <= 9)
+	{
+		_putchar(i);
+		_putchar(',');
+		_putchar(' ');
+		i++;
+	}
+	_putchar('\n');
+	n++;
+}
+}
