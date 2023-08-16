141d97da564572c5ab92085b6e26c249fc8189f3
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-15 19:34:42 -0700

    v0 t8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *jack_bauer - the main function to print absalute
+ *n: the return is integr value
+ *description: this function to print absalute value
+ *Return: 0 for absalute value
+ */
+void jack_bauer(void)
+{
+	int no = 0;
+	int no2;
+
+	while (no < 24)
+	{
+		no2 = 0;
+		while (no2 < 60)
+		{
+			_putchar(no / 10 + '0');
+			_putchar(no % 10 + '0');
+			_putchar(':');
+			_putchar(no2 / 10 + '0');
+			_putchar(no2 % 10 + '0');
+			no2++;
+		}
+		no++;
+	}
+}
