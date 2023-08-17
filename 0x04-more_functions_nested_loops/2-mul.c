3b6e495e2d3ef7cf5b317df08fc00e9cf2e8166d
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-17 08:07:38 -0700

    v0 is t2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_isdigit - for check digits
+ *description: this function for digits check
+ *@a: the digit for usage
+ *@b: the second digit
+ *Return: 1 if true and 0 if not
+ */
+int mul(int a, int b)
+{
+	int x = a + b;
+return (x);
+}
