e3a83befa0b35e77b5bd8a0a992fdcdbffcebc5a
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-18 15:59:34 -0700

    v0 fizz t9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - for printing fizbuz play
+ *description:  printing fizz when multi 3 or buzz for 5 multi or fizzbuzz
+ *Return: 0 success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 100; x++)
+	{
+		if (x % 3 == 0 && (x % 15 != 0))
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0 && (x % 15 != 0))
+		{
+			printf("Buzz ");
+		}
+		else if (x % (3 * 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
