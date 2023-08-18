80c90a1e59d9ef958fdf0a59214449dfc60beb23
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-17 20:20:19 -0700

    v0 t6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_line - for printing  line
+ *@n: for nomber
+ *description: this function for printing series of lines
+ *Return: void
+ */
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x <= n; x++)
+	{
+		_putchar('_');
+	}
+		_putchar('\n');
+
+}
