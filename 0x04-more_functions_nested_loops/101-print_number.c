24c443d0eef6c88787a62345f2688de707d04e57
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-19 02:42:08 -0700

    v0 printno t101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_numbers - for printing series no
+ *@n: no to be printed
+ *description: this function for print nomber
+ *Return: void
+ */
+void print_number(int n)
+{
+
+		_putchar(x + '0');
+		_putchar('\n');
+}
