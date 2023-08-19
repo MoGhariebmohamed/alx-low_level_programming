6246e24168816252c268ea3af93c80d724f40c39
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-18 22:34:42 -0700

    v5 poss t9

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,28 +1,28 @@
 #include "main.h"
 /**
- *print_diagonal - for printing diagonal
- *@n: for nomber
- *description: this function for printing series of slahes as a diagonal
+ *print_triangle - for printing triangle
+ *@size: for nomber size of the triangle
+ *description: this function for printing triangle
  *Return: void
  */
 void print_triangle(int size)
 {
 	int z;
 	int x;
-if (n > 0)
-{
-	for (z = 0; z < n; z++)
-	{
-		for (x = 0; x < z; x++)
-			_putchar(' ');
-		_putchar('\\');
 
-		if (z == n - 1)
-			continue;
+	for (z = 1; z < size; z++)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			if (x < size - z)
+			_putchar(' ');
+		}
+		else if	(z >= size - z)
+		{
+			_putchar('#');
 
 		_putchar('\n');
-	}
+		}
 }
 _putchar('\n');
 }
-
