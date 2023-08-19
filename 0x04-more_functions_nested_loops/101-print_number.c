4975a5cd6714012e8ec54a390cebe49e8bea06e7
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-19 03:05:57 -0700

    v1 printno t101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,14 @@
  */
 void print_number(int n)
 {
+	unsigned int x = n;
 
-		_putchar(x + '0');
-		_putchar('\n');
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
