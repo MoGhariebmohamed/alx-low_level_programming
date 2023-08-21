8607e9164a6f6e455a283a0879792acda88b5d4c
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-21 14:36:48 -0700

    v0 t0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *reset_to_98 - for reset value with position
+ *description: this function for charachter check
+ *@n: the charachter to be replaced
+ *Return: 1 if true and 0 if not
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 89;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef  MAIN_H
+#define MAIN
+
+#include <stdio.h>
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
