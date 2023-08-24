eea026738f6408577059788bc583df414e55e7c6
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-24 13:05:25 -0700

    v0 t9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strcpy - for change array content
+ *description: this function for print half string new line
+ *@src: the char  pointer for a string location
+ *@dest: the new position
+ *Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = -1; src[x] != '\0'; x++)
+	{
+		dest[x]  = src[x];
+		if (src[x] != '\0')
+			continue;
+	}
+		return (dest);
+}
