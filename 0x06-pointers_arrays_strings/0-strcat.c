dd2350df4a143e49ecfdc04de44731f973b15492
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-23 18:13:02 -0700

    v0 tsk0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strcat - for reset value
+ *description: this function for charachter check
+ *@dest: the int pointer for a
+ *@src: the pointer for b
+ *Return: void
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int x;
+	int y;
+
+	for (x = 0; dest[c]; x++)
+		;
+	for (y = 0; srec[y]; y++)
+
+	dest[c] = src[y];
+	c++;
+	return (dset);
+}
