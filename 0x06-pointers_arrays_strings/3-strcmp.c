378f57ff0af5ce0a7916e48c6fc3bcab0b0cda66
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-24 18:57:34 -0700

    v0 t3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strcmp - for cpy the string
+ *description: this function for conactes two strings
+ *@s1: the int pointer for a first string
+ *@s2: the pointer for second string
+ *Return: int sucess
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			x = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (x);
+}
