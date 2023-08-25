36412f09a56e8d88ecd87668dfea6e82a93bc76a
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-24 17:59:34 -0700

    v1 t1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,9 +14,11 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (x = 0; dest[x]; x++)
 		;
-	for (y = 0; y < n && src[y] != '0'; y++)
+	for (y = 0; y < n && src[y] != '\0'; y++)
+	{
+		dest[x + y] = src[y];
 
 	dest[x + y] = '\0';
-	x++;
+	}
 	return (dest);
 }
