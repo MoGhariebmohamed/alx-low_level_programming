730a9b506238bacaebc6b065297ba04e2bbcacb4
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-27 07:51:49 -0700

    v2 t100

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*main - generate kegen for program
+*description: this program to generate password for prog
+*Return: 0 success
+*/
+
+int main(void)
+{
+
+return (0);
+}
