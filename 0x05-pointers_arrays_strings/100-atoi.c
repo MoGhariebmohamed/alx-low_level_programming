3c733f41db96070b92ec27b88955598063066a57
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-27 06:56:57 -0700

    v0 t100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_atoi - for copy  elements of array
+ *description: this function for copy elemnts of src array
+ *@s: the char  pointer for an array
+ *Return: int  success
+ */
+
+int _atoi(char *s)
+{
+	int x = 1;
+	unsigned int y = 0;
+
+	do {
+		if (*s == '-')
+		{
+			x = x * (-1);
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			y = (*s - 48);
+		}
+		else if (y > 0)    /*as an ascii point not 0 */
+			break;
+	} while (*s++);
+
+	return (x * y);
+}
