b091e2b6372d788dbd8a4dcc59088bfa5b903689
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-28 16:01:53 -0700

    v0 t4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_strpbrk - for search  of string
+ *description: this function for searching the stringfor any bytes
+ *@s: the string to be printed
+ *@accept: the location of the sub string
+ *Return: char  succes
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int x;
+	int y;
+	char *p;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; accept[y] != s[x]; y++)
+		{
+			if (accept[y] == s[x])
+			{
+				p = &s[x];
+				return (p);
+			}
+		}
+	}
+	return (0);
+}
