24536153f6a21af2e469121901ab0243c6afe35b
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-28 15:32:20 -0700

    v2 t3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,23 +3,21 @@
  *_strspn - for length of string
  *description: this function for locating char in string
  *@s: the string to be printed
- *@accept: the location of the string
- *Return: c  succes or null if not found
+ *@accept: the location of the sub string
+ *Return: unsigned int  succes 
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	int x;
 	int y;
 
-	for (x = 0; s[x]  >= '\0'; x++)
+	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (y = 0; accept[y] >= '\0'; y++)
+		for (y = 0; accept[y] != s[x]; y++)
 		{
-				if (accept[y] == '\0')
-				{
-					return (x);
-				}
+			if (accept[y] == '\0')
+				return (x);
 		}
 	}
-		return (x);
+	return (x);
 }
