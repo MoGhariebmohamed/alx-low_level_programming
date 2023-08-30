4364c1ad5b0915202a86d20b6bca10f124c19783
mohamed gharieb <m.gharieb89@gmail.com>
2023-08-30 04:55:56 -0700

    v0 t3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *factorial - for factorial of n
+ *description: this function for return facttorial of n
+ *@n: string pointer
+ *Return: int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return(1);
+	}
+	else
+	{
+		return ( n * factorial(n - 1));
+	}
+}
