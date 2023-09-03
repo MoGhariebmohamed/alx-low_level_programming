0807d4d93a6c0b768edf82283999e04e25c0d88d
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-03 03:54:54 -0700

    v0 t3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *main - for printing file name
+ *description: this function for printing the file neme each time change
+ *@argv: the string for argv
+ *@argc: the counter for string
+ *Return: 0 if not
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int x;
+	int y;
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		i = x * y;
+		printf("%d\n", i);
+	}
+		return (0);
+}
