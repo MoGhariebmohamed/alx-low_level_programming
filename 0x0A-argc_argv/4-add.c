4e425d8a89a8ae7abf7ab44005403a7c14a0c988
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-04 04:40:05 -0700

    v5 t4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,26 +12,21 @@ int main(int argc, char *argv[])
 {
 	int i = 1;
 	int j;
-	char x = *argv[i];
-	int total;
+	int total = 0;
 
-	for (i = 1; i <= argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; j <= argv[i][j]; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (x >= '0' && x <= '9')
-			{
-
-				total += atoi(argv[i]);
-			}
-			else
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
+
 			}
 		}
+		total += atoi(argv[i]);
 	}
 		printf("%d\n", total);
-	
 		return (0);
 }
