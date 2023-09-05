8e78a21ba8236dcff8a4a19b400d87f71c9104b2
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-05 11:43:11 -0700

    v1 t1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_strdup - for creat a memor for double string
+ *description: this function for creat a space in memor
+ *@str: the string size
+ *Return: NULL if c is 0 or fail else the array
+ */
+char *_strdup(char *str)
+{
+	char *dstring;
+	int x;
+	int y;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		dstring = malloc(sizeof(*str) * x);
+	}
+	if (dstring == NULL)
+		return (NULL);
+	else 
+	{
+		for (y = 0; y <= x; y++)
+		{
+			dstring[y] = str[x];
+		}
+	}
+	return (dstring);
+}
