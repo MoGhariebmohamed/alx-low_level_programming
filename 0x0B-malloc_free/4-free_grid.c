d4f0282bdbe4f2e8482b47bb09fc2574e23130de
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-06 11:42:22 -0700

    v0 t4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *free_grid - for free an array of int
+ *description: this function for free array of aLOC
+ *@grid:  array
+ *@height: array height
+ *Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = 0; x <= height; x++)
+		free(grid[x]);
+	free(grid);
+}
