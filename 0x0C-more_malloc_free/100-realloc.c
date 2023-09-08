49d7d4ebe0f5cfd75fb6f75d885f3971f28af71b
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-08 15:04:24 -0700

    v1 t100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_realloc - for reallocate an array
+ *description: this function for reallocate an aarray
+ *@ptr: the pointer
+ *@old_size: the array old size
+ *@new_size: the new size for array
+ *Return: NULL if c is 0 or fail else the array
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *arr;
+	int *newarr;
+	int *newptr;
+	unsigned int x;
+
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (ptr == NULL)
+	{
+		arr = malloc(new_size);
+		if (arr == NULL)
+		{
+			return (NULL);
+		}
+		return (arr);
+	}
+	newptr = ptr;
+	arr = malloc(sizeof(newptr) * new_size);
+	if (arr == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	newarr = arr;
+	for (x = 0; x < old_size && x < new_size; x++)
+	newarr[x] = newptr[x];
+	free(ptr);
+	return (arr);
+}
