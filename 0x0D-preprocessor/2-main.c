b9d300c8b9ee768824056637636c96c8495169a0
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-11 11:58:40 -0700

    v1 t2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - function to print file name
+ *description: this function to print out file name
+ *Return: 0
+ */
+int main (void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
