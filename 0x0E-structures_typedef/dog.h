cdc517abc353e3305c9e59e2ef78382c20e33d82
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-11 19:58:05 -0700

    v0 t0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ *struct dog - for do structure
+ *description: this function for structure dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
