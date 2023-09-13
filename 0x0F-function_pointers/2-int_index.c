166df424254a3af84e882f14729b1ba19ea4e68d
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-13 14:12:32 -0700

    v1 t2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ *int_index - for excute an array
+ *description: function executes as a parameter on each element of an array
+ *@array: pointer to an array
+ *@size: size of array
+ *@cmp: function pointer to be used to compare values
+ *Return: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array == NULL || cmp == NULL)
+		return(-1);
+
+	for (x = 0; x < size; x++)
+	{
+	if (cmp(array[x])!= 0)
+		return (x);
+	}
+	return (-1);
+}
