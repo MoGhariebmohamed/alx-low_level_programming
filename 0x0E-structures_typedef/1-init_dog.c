82494a6b2a8d9df0abb650f405bedff9c1cf10ed
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-12 18:14:26 -0700

    v1 t1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *init_dog - for variables initiation
+ *description: this function for allocate memory using malloc
+ *@d: the dog name
+ *@name: dog name
+ *@age: dog age
+ *@owner: dog owner
+ *Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	if (d != NULL)
+	{
+		(*d).name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
