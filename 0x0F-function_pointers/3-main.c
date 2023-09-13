1837f5d97a25adee7be79ae49cc850467ddd5a5f
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-13 16:48:54 -0700

    v5 t3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' && a == 0) || (argv[2][0] == '%' && b == 0))
+	if ((argv[2][0] == '/' && b == 0) || (argv[2][0] == '%' && b == 0))
 	{
 		printf("Error\n");
 		exit(100);
