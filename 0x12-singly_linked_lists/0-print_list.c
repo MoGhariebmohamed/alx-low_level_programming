86d278ef42e946a3260a15ab19cc2523e35907d8
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-23 14:00:33 -0700

    v3 t0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,41 +1,41 @@
 #include "lists.h"
 /**
  *getlength - to get string length
- *@str: a pointer to string
+ *description: the function to get length of a string
+ *@string: a pointer to string
  *Return: string length
  */
 int getlength(char *string)
 {
-	int x = 0;
+	int x;
 
 	if (string == NULL)
 	return (0);
-	for (x = 0; string[x] != '\0'; x++)
+	for (x = 1; string[x] != '\0'; x++)
 	{
 		x += 1;
 	}
 		return (x);
-	
+
 }
 /**
- *print_list - for print a list 
+ *print_list - for print a list
  *description: this function for print a the elements of a list_t list.
- *@s: the list pointer
- *Return: number if nodes if exist 
+ *@h: the list pointer
+ *Return: number if nodes if exist
  */
 size_t print_list(const list_t *h)
 {
 	size_t x = 0;
 
-	if (h->str == NULL)
-	{
-		printf("[%d] %s\n", getlength(h->str), "nil");
-	}
 	for (x = 0; h->str != NULL; x++)
 	{
 		printf("[%d] %s\n", getlength(h->str), h->str);
 		h = h->next;
 	}
-	
+	if (h->str == NULL)
+	{
+		printf("[%d] %s\n", getlength(h->str), "nil");
+	}
 	return (x);
 }
