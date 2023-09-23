c05060a974b9844175218a6907f6296e7064234a
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-23 08:25:23 -0700

    v8 to

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,21 +2,49 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ *_putchar - to put one char
+ *@c: charachter
+ *Return: the characher of string
+ */
 int _putchar(char c);
+/**
+ *sum_them_all - to summ all variables
+ *@n: integer variable initial
+ *Return: integer if done
+ */
 int sum_them_all(const unsigned int n, ...);
+/**
+ *print_numbers - to print all numbers
+ *@separator: the first seperator for string
+ *@n: nober of strings
+ *Return: nothing
+ */
 void print_numbers(const char *separator, const unsigned int n, ...);
+/**
+ *print_string - to print string
+ *@separator: the first seperator for string
+ *@n: nober of strings
+ *Return: nothing
+ */
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ *print_all - to print all var
+ *@format: pointer constant to string
+ *Return: nothing
+ */
 void print_all(const char * const format, ...);
 
 /**
- *struct typ - Struct operators
+ *struct fun - Struct operators
  *description: this structure to get all function
- * @id: Char
- * @all: The function associated
+ *@get: a function to get
+ *@id: Char pointer
 */
 typedef struct fun
 {
 	char *id;
 	void (*get)(va_list all);
-}fun_t;
+} fun_t;
+
 #endif
