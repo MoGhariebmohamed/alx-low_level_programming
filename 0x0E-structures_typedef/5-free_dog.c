9a5850840ee342b068331fc229cdf5305e14a83e
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-23 07:26:16 -0700

    v3 t5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,38 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *print_dog - for print dog structue
- *description: this function for prints a struct dog
- *@d: the dog name
+ *free_dog - for free dog structue
+ *description: this function for free a struct dog
+ *@d: the dog pointer
  *Return: void
  */
-void print_dog(struct dog *d)
+void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return;
-
-	if ((*d).name == NULL)
+	if (d != NULL)
 	{
-		printf("Name: (nil)\n");
-	}
-	else
-	{
-		printf("Name: %s\n", d->name);
-	}
-	if (d->age < 0)
-	{
-		printf("Age: (nil)\n");
-	}
-	else
-	{
-		printf("Age: %f\n", (*d).age);
-	}
-	if (d->owner == NULL)
-	{
-		printf("Owner: (nil)\n");
-	}
-	else
-	{
-		printf("Owner: %s\n", (*d).owner);
+		if ((*d).name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+		free(d);
 	}
 }
