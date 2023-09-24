bb2750a8346bef7ce01850df3de89b8792cbb093
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-24 08:05:04 -0700

    v01 t2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,8 +17,8 @@ list_t *add_node(list_t **head, const char *str)
 	duplicate = strdup(str);
 	if (node == NULL || duplicate == NULL)
 	{
+	  	free(node);
 		return (NULL);
-		free(node);
 	}
 
 	for (x = 0; str[x] != '\0'; x++)
