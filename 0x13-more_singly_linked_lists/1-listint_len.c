197505e83128f8853674f4035f3b837787421396
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-25 14:27:04 -0700

    v1 t1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *listint_len - for print a length of list of int
+ *description: this function for return the elements of a listint_t list.
+ *@h: the list pointer
+ *Return: number if nodes if exist
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t x;
+
+	for (x = 0; h != NULL; x++)
+	{
+		 h = h->next;
+	}
+	return (x);
+}
