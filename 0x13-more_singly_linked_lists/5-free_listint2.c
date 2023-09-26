dbe0c2c07b250f6dde9054467b1c5d3437bdb45e
mohamed gharieb <m.gharieb89@gmail.com>
2023-09-26 10:08:10 -0700

    v5 t5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *free_listint - for free of list of int
+ *description: this function for free the elements of a listint_t list.
+ *@head: the list pointer
+ *Return: nothing
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *x;
+	listint_t *alth;
+	unsigned int i;
+
+	if (head == NULL)
+		return;
+
+	alth = *head;
+	for (i = 0; alth != NULL; i++)
+	{
+		x = alth;
+		alth = alth->next;
+		free(x);
+	}
+
+}
