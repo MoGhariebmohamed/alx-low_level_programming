aadf767a9c7bcec3e58346415aadbb14a404fd5b
mohamed gharieb <m.gharieb89@gmail.com>
2023-10-02 10:56:33 -0700

    v3 t0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 /**
  *read_textfile - for change reads a text file and prints it to the POSIX
- *description: this function for that reads a text file and prints it to the POSIX
+ *description: this function for that reads a text file and prints it
  *@filename: the file name
  *@letters:  number of letters it should read and print
  *Return: number unsigned or 0
@@ -15,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t openfile;
 	char *x;
 
-	if (filename == NULL)
+	if (letters == '\0' || filename == NULL)
 		return (0);
 	x = malloc(sizeof(char) * letters);
 	if (x == NULL)
@@ -25,14 +27,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (openfile == -1)
 	{
 		free(x);
-                return (0);
+		return (0);
 	}
 
 	openfile = read(openfile, x, letters);
 	if (openfile == -1)
 	{
 		free(x);
-                return (0);
+		return (0);
 	}
 	openfile = write(STDOUT_FILENO, x, openfile);
 	if (openfile == -1)
