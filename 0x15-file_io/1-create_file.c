3c4e97f1baa8738297babfcd19b0cee9ed132e93
mohamed gharieb <m.gharieb89@gmail.com>
2023-10-02 16:53:02 -0700

    v5 t1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		x = 0;
-		while (filename[x] != '\0')
+		while (text_content[x] != '\0')
 		{
 			x++;
 		}
