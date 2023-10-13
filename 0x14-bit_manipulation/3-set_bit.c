f5a4d33561690d737b4e0252c7847899da5f82e0
mohamed gharieb <m.gharieb89@gmail.com>
2023-10-13 08:09:40 -0700

    t3 v0

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *set_bit - for return value of a bit at a given index
+ *description: this function for value of a bit at a given index
+ *@n: the binary  pointer
+ *@index: the index, starting from 0 of the bit you want to get
+ *Return: value of the bit at index index or -1 if an error occured
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index >= ((sizeof(n) - 1) * 8))
+		return (-1);
+
+	*n = *n | (1 << index);
+
+	return (1);
+
+}
