a4993fd3c1f33388174241b6b6a41ecd842fdf07
Mohamed <m.gharieb@gmail.com>
2023-10-17 13:26:15 -0700

    t5 v1

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *flip_bits - for return returns the number of bits 
+ *description: this function for returns the number of bits 
+ *@n: the binary  pointer
+ *@m: the index, starting from 0 of the bit you want to get
+ *Return: value of the bit at index index or -1 if an error occured
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int x, index = 0;
+
+	for (x = ((sizeof(n) - 1) * 8); x >= 0; x--)
+	{
+		if (((n ^ m) >> x) & 1)
+		{
+		index++;
+		}
+	}
+	return (index);
+
+}
