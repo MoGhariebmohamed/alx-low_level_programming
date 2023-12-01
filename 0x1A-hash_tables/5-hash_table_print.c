6f6d879f687cbaf0d0299bd6e6a5cc3d9bacb3ec
mohamed gharieb <m.gharieb89@gmail.com>
2023-12-01 14:26:07 -0800

    t5 v2

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ *hash_table_print -  that prints a hash table.
+ *@ht: the hash table
+ *Return: the value associated with the element, or NULL
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned int x, y;
+    hash_node_t *mynode;
+    
+    if (ht == NULL)
+    return;
+    printf("{");
+    for (x = 0; x < ht->size; x++)
+    {
+        mynode = ht->array[x];
+        while (mynode != NULL)
+        {
+            if (y != 0)
+            printf(", ");
+            y = 1;
+    printf("'%s':'%s'", mynode->key, mynode->value);
+    mynode = mynode->next;
+    }
+    }
+    printf("}\n");
+}
