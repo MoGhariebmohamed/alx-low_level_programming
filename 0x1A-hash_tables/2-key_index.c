5fa3ee5691c39d39445c391a1f1e8c0fc190288e
mohamed gharieb <m.gharieb89@gmail.com>
2023-12-01 09:45:22 -0800

    t2 v1

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,20 +3,13 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
+ *key_index - gives the index of a key
+ *@key: the key of
+ *@size: size of the array of the hash table
+ *Return: ndex at which the key/value pair should be stored
  */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
 
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
 }
